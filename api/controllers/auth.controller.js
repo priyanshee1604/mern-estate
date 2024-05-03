a077f9d3ca60519c3c26536aa559f5e22713d3ca
Priyanshee <priyansheesisodiya1604@gmail.com>
2024-05-03 14:22:59 +0530

    create sign up api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    //using await as save takes time to execute thus to prevent it from blocking code we use await.
+    //and thus we need to make the signup function async.
+    //save used to save data in the database.
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
-
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -13,8 +13,12 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+
+app.use(express.json()); //allow json as input of the server
+
 app.listen(3000, () => {
   console.log(`server is running on port 3000!!`);
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
